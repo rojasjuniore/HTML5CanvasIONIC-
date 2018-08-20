5e64310ac8a30fa8beb21c12a428e983ab17b024
Junior Rojas <developer@bnf.com.co>
2018-08-20 12:05:23 -0500

    first commit

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { SmileRateComponent } from './smile-rate/smile-rate';
+@NgModule({
+	declarations: [SmileRateComponent],
+	imports: [],
+	exports: [SmileRateComponent]
+})
+export class ComponentsModule {}
